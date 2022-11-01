5896c944e3b88ba47b9243269846be9d2007a4ea
viji4deepi <viji4deepi@gmail.com>
2022-11-01 17:26:35 +0530

    serverlet error solved

--- recipeApp/src/main/java/org/launchcode/liftoff/recipeApp/RecipeAppApplication.java
+++ recipeApp/src/main/java/org/launchcode/liftoff/recipeApp/RecipeAppApplication.java
@@ -3,6 +3,9 @@ package org.launchcode.liftoff.recipeApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,13 +17,22 @@ public class RecipeAppApplication {
 	}
 
 	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*");
-			}
-		};
+	public CorsFilter corsFilter() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration configuration = new CorsConfiguration();
+
+		configuration.setAllowCredentials(true);
+		configuration.addAllowedOriginPattern("*");
+		configuration.addAllowedHeader("*");
+		configuration.addAllowedMethod("OPTIONS");
+		configuration.addAllowedMethod("HEAD");
+		configuration.addAllowedMethod("GET");
+		configuration.addAllowedMethod("PUT");
+		configuration.addAllowedMethod("POST");
+		configuration.addAllowedMethod("DELETE");
+		configuration.addAllowedMethod("PATCH");
+		source.registerCorsConfiguration("/**",configuration);
+		return new CorsFilter(source);
 	}
 
 }
